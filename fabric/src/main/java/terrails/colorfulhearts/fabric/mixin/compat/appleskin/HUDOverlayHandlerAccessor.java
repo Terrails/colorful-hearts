6d2b83d3ba783c63fadd68f08c7c21ea632a2527
Terrails <github@sl.terrails.com>
2023-10-22 16:31:23 +0200

    do not remap fields

--- fabric/src/main/java/terrails/colorfulhearts/fabric/mixin/compat/appleskin/HUDOverlayHandlerAccessor.java
+++ fabric/src/main/java/terrails/colorfulhearts/fabric/mixin/compat/appleskin/HUDOverlayHandlerAccessor.java
@@ -4,7 +4,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import squeek.appleskin.client.HUDOverlayHandler;
 
-@Mixin(HUDOverlayHandler.class)
+@Mixin(value = HUDOverlayHandler.class, remap = false)
 public interface HUDOverlayHandlerAccessor {
 
     @Accessor float getFlashAlpha();
