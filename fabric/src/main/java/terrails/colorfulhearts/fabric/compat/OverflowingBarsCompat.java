719142bfe97fdfe006907817731da832d4edd299
Terrails <github@sl.terrails.com>
2025-07-30 02:36:51 +0200

    add overflowing bars as an actual dependency and do not use reflection

--- fabric/src/main/java/terrails/colorfulhearts/fabric/compat/OverflowingBarsCompat.java
+++ fabric/src/main/java/terrails/colorfulhearts/fabric/compat/OverflowingBarsCompat.java
@@ -1,7 +1,12 @@
 package terrails.colorfulhearts.fabric.compat;
 
+import fuzs.overflowingbars.OverflowingBars;
+import fuzs.overflowingbars.client.gui.RowCountRenderer;
+import fuzs.overflowingbars.config.ClientConfig;
 import terrails.colorfulhearts.api.fabric.event.FabHeartEvents;
 import terrails.colorfulhearts.compat.OverflowingBarsCommonCompat;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 
 public class OverflowingBarsCompat extends OverflowingBarsCommonCompat {
 
@@ -10,5 +15,15 @@ public class OverflowingBarsCompat extends OverflowingBarsCommonCompat {
         if (drawBarRowCount != null) {
             FabHeartEvents.POST_RENDER.register(this::render);
         }
+    }
+
+    @Override
+    protected void drawBarRowCount(GuiGraphics guiGraphics, int posX, int posY, int barValue, int maxRowCount) {
+        RowCountRenderer.drawBarRowCount(guiGraphics, posX, posY, barValue, true, maxRowCount, Minecraft.getInstance().font);
+    }
+
+    @Override
+    protected boolean allowCount() {
+        return OverflowingBars.CONFIG.get(ClientConfig.class).health.allowCount;
     }
 }
--- neoforge/src/main/java/terrails/colorfulhearts/neoforge/compat/OverflowingBarsCompat.java
+++ neoforge/src/main/java/terrails/colorfulhearts/neoforge/compat/OverflowingBarsCompat.java
@@ -1,20 +1,33 @@
 package terrails.colorfulhearts.neoforge.compat;
 
+import fuzs.overflowingbars.OverflowingBars;
+import fuzs.overflowingbars.client.gui.RowCountRenderer;
+import fuzs.overflowingbars.config.ClientConfig;
 import net.neoforged.neoforge.common.NeoForge;
 
 import terrails.colorfulhearts.api.neoforge.event.NeoHeartRenderEvent;
 import terrails.colorfulhearts.compat.OverflowingBarsCommonCompat;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 
 public class OverflowingBarsCompat extends OverflowingBarsCommonCompat {
 
     public OverflowingBarsCompat() {
         super();
-        if (drawBarRowCount != null) {
-            NeoForge.EVENT_BUS.addListener(this::onPostRender);
-        }
+        NeoForge.EVENT_BUS.addListener(this::onPostRender);
     }
 
     public void onPostRender(NeoHeartRenderEvent.Post event) {
         this.render(event.getEvent());
+    }
+
+    @Override
+    protected void drawBarRowCount(GuiGraphics guiGraphics, int posX, int posY, int barValue, int maxRowCount) {
+        RowCountRenderer.drawBarRowCount(guiGraphics, posX, posY, barValue, true, maxRowCount, Minecraft.getInstance().font);
+    }
+
+    @Override
+    protected boolean allowCount() {
+        return OverflowingBars.CONFIG.get(ClientConfig.class).health.allowCount;
     }
 }
