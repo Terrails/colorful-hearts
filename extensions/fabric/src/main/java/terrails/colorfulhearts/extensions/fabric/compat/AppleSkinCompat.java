a56e66a34b3b4b559bda0dcd1af8bfa0a6723f2a
Filip <github@sl.terrails.com>
2026-06-13 01:18:20 +0900

    fix fabric appleskin compat

--- extensions/fabric/src/main/java/terrails/colorfulhearts/extensions/fabric/compat/AppleSkinCompat.java
+++ extensions/fabric/src/main/java/terrails/colorfulhearts/extensions/fabric/compat/AppleSkinCompat.java
@@ -1,34 +1,47 @@
 package terrails.colorfulhearts.extensions.fabric.compat;
 
+import squeek.appleskin.ModConfig;
 import squeek.appleskin.api.AppleSkinApi;
-import squeek.appleskin.api.event.HUDOverlayEvent;
 import squeek.appleskin.client.HUDOverlayHandler;
+import squeek.appleskin.helpers.ConsumableFood;
+import squeek.appleskin.helpers.FoodHelper;
 
 import terrails.colorfulhearts.api.fabric.event.FabHeartEvents;
+import terrails.colorfulhearts.api.heart.drawing.OverlayHeart;
 import terrails.colorfulhearts.extensions.compat.AppleSkinCommonCompat;
 import terrails.colorfulhearts.extensions.fabric.mixin.appleskin.HUDOverlayHandlerAccessor;
+import net.minecraft.client.Minecraft;
 import net.minecraft.util.Mth;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodData;
 
 public class AppleSkinCompat extends AppleSkinCommonCompat implements AppleSkinApi {
 
-    private int modifiedHealth;
-
     @Override
     public void registerEvents() {
-        HUDOverlayEvent.HealthRestored.EVENT.register(event -> {
-            // on forge this event never executes as it depends on another event that never gets called, but it does here
-            // we can take advantage of this and take the value of modifiedHealth without having to calculate it
-            event.isCanceled = true;
-            modifiedHealth = Mth.ceil(event.modifiedHealth);
-        });
         // register own custom renderer and use modifiedHealth that AppleSkin's event provided
         FabHeartEvents.POST_RENDER.register(event -> {
-            Player player = client.player;
-            assert player != null;
+            Minecraft client = Minecraft.getInstance();
+            Player player = event.getPlayer();
 
-            int health = Mth.ceil(player.getHealth());
-            if (modifiedHealth <= health) {
+            if (!shouldDrawOverlay(event.getOverlayHeart().orElse(null), player)) {
+                return;
+            }
+
+            /* copied from HUDOverlayHandler */
+            FoodHelper.QueriedFoodResult result = ((HUDOverlayHandlerAccessor) HUDOverlayHandler.INSTANCE).getHeldFood().result(client.gui.getGuiTicks(), player);
+            if (result == null) {
+                HUDOverlayHandler.INSTANCE.resetFlash();
+                return;
+            }
+
+            float foodHealthIncrement = FoodHelper.getEstimatedHealthIncrement(player, new ConsumableFood(result.modifiedFoodComponent, result.consumableComponent));
+            float currentHealth = player.getHealth();
+            float modifiedHealth = Math.min(currentHealth + foodHealthIncrement, player.getMaxHealth());
+
+            if (modifiedHealth <= currentHealth) {
                 return;
             }
 
@@ -38,11 +51,36 @@ public class AppleSkinCompat extends AppleSkinCommonCompat implements AppleSkinA
             // I'll leave this behaviour as is at it makes the differentiation easier
             float alpha = ((HUDOverlayHandlerAccessor) HUDOverlayHandler.INSTANCE).getFlashAlpha();
 
-            drawHealthOverlay(event.getGuiGraphics(), event.getX(), event.getY(), absorbing, health, modifiedHealth, alpha, event.isHardcore());
-            // set value back to 0
-            modifiedHealth = 0;
+            drawHealthOverlay(event.getGuiGraphics(), event.getX(), event.getY(), absorbing, Mth.ceil(currentHealth), Mth.ceil(modifiedHealth), alpha, event.isHardcore());
         });
 
         FabHeartEvents.UPDATE.register(() -> this.lastHealth = 0);
+    }
+
+    public boolean shouldDrawOverlay(OverlayHeart overlayHeart, Player player) {
+        if (overlayHeart != null) {
+            return false; // AppleSkin usually checks the effect, but we'll do it this way
+        }
+
+        /* copied from HUDOverlayHandler */
+        if (!ModConfig.INSTANCE.showFoodHealthHudOverlay) {
+            return false;
+        }
+
+        // in the `PEACEFUL` mode, health will restore faster
+        if (player.level().getDifficulty() == Difficulty.PEACEFUL)
+            return false;
+
+        FoodData stats = player.getFoodData();
+
+        // when player has any changes health amount by any case can't show estimated health
+        // because player will confused how much of restored/damaged healths
+        if (stats.getFoodLevel() >= 18)
+            return false;
+
+        if (player.hasEffect(MobEffects.REGENERATION))
+            return false;
+
+        return true;
     }
 }
